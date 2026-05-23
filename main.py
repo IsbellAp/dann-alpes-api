9e74e6a6d570a5125308545c21d273ad8029df77
IsbellAp <i.casallas@uniandes.edu.co>
2026-05-23 17:57:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,14 @@ from datetime import datetime
 import os
 
 app = FastAPI()
-#hola
 
-
-from fastapi.middleware.cors import CORSMiddleware
-
-@app.middleware("http")
-async def add_cors_headers(request, call_next):
-    response = await call_next(request)
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "*"
-    return response
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 MONGO_URL = os.environ.get("MONGO_URL")
 client = MongoClient(MONGO_URL)
